24c312c25b8cd5440024dcb41ac9ac7ac258b833
RStep1 <stepanrykov.itmo@gmail.com>
2025-06-14 21:07:19 +0300

    test: refactor BasePage

--- lab-3/java-tests/src/test/java/com/mycompany/pages/BasePage.java
+++ lab-3/java-tests/src/test/java/com/mycompany/pages/BasePage.java
@@ -6,13 +6,30 @@ import org.openqa.selenium.support.PageFactory;
 import org.openqa.selenium.support.ui.*;
 
 public abstract class BasePage {
-    protected WebDriver driver;
-    protected WebDriverWait wait;
+    private WebDriver driver;
+    private WebDriverWait wait;
 
     protected BasePage(WebDriver driver) {
         this.driver = driver;
         this.wait = new WebDriverWait(driver, Duration.ofSeconds(10));
         PageFactory.initElements(driver, this);
+    }
+
+    public BasePage acceptAllPrivacy() {
+        try {
+            WebElement shadowHost = wait.until(ExpectedConditions.presenceOfElementLocated(
+                By.cssSelector("#usercentrics-root")
+            ));
+            SearchContext shadowRoot = shadowHost.getShadowRoot();
+            WebElement acceptButton = shadowRoot.findElement(
+                By.cssSelector("#uc-center-container > div.sc-eBMEME.gMirTG > div > div > div > button.sc-dcJsrY.eLOIWU")
+            );
+            acceptButton.click();
+            wait.until(driver -> !shadowHost.isDisplayed());
+        } catch (Exception ignored) {
+        }
+
+        return this;
     }
 
     protected void click(WebElement element) {
@@ -39,6 +56,14 @@ public abstract class BasePage {
 
     protected void waitForInvisibility(By locator) {
         wait.until(ExpectedConditions.invisibilityOfElementLocated(locator));
+    }
+
+    protected WebDriver getDriver() {
+        return driver;
+    }
+
+    protected WebDriverWait getWait() {
+        return wait;
     }
 }
 
