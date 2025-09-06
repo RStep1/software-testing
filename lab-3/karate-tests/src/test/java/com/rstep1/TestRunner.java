c7c2219fafffb33465a22cdd955f57bf443503f1
RStep1 <stepanrykov.itmo@gmail.com>
2025-09-06 12:41:10 +0300

    add allure reports for karate tests

--- lab-3/karate-tests/src/test/java/com/rstep1/TestRunner.java
+++ lab-3/karate-tests/src/test/java/com/rstep1/TestRunner.java
@@ -2,6 +2,9 @@ package com.rstep1;
 
 import com.intuit.karate.Results;
 import com.intuit.karate.Runner;
+
+import io.qameta.allure.karate.AllureKarate;
+
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
@@ -18,6 +21,9 @@ class TestRunner {
     @Test
     void testAll() {
         Results results = Runner.path("classpath:karate/features")
+                .hook(new AllureKarate())
+                .outputCucumberJson(true)
+                .outputJunitXml(true)
                 .parallel(1);
         assertEquals(0, results.getFailCount(), results.getErrorMessages());
     }
